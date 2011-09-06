570896853b21e74b60e0184512c3be3797ab2a37
Henrik <henrik@haf.se>
2011-09-06 13:20:05 +0200

    inital import

---
+++ ParallelExample/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Stateless;
+
+namespace ParallelExample
+{
+    public static class C
+    {
+        static SequentialActionQueue q = new SequentialActionQueue();
+
+        public static void WriteLine(ConsoleColor color, string format, params object[] items)
+        {
+            q.Enqueue(() =>
+                {
+                    var oldColor = Console.BackgroundColor;
+                    Console.BackgroundColor = color;
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine(format, items);
+                    Console.BackgroundColor = oldColor;
+                }); 
+        }
+    }
+
+    public class MyAsyncStateMachine
+    {
+        static string on = "On", off = "Off";
+        static char space = ' ';
+        static Random rnd = new Random();
+
+        ConsoleColor color;
+
+        StateMachine<string, char> stateMachine;
+
+        SequentialActionQueue queue = new SequentialActionQueue();
+        
+        public MyAsyncStateMachine()
+        {
+            this.color = (ConsoleColor)rnd.Next(15);
+            this.stateMachine = CreateMachine();
+            this.queue.PropertyChanged += (o, e) => C.WriteLine(this.color, "{2} {0} = {1}", e.PropertyName, this.queue.IsActive, this.stateMachine.Id);
+        }
+
+        public void Fire(char trigger, int i)
+        {
+            queue.Enqueue(() =>
+            {
+                C.WriteLine(this.color, "actually starting {0}", i);
+                stateMachine.Fire(trigger);
+            });
+        }
+
+        private StateMachine<string, char> CreateMachine()
+        {
+            var machine = new StateMachine<string, char>(off);
+
+            Action log = () =>
+            {
+                C.WriteLine(this.color, "switched pre {1}: {0}", machine.Id, machine.State);
+                //Thread.Sleep(1000);
+                C.WriteLine(this.color, "switched post {1}: {0}", machine.Id, machine.State);
+            };
+
+            machine
+                .Configure(off)
+                .Permit(space, on)
+                .OnEntry(log);
+            machine
+                .Configure(on)
+                .Permit(space, off)
+                .OnEntry(log);
+            return machine;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = CreateMachines(3);
+            
+            Parallel.ForEach(results, m =>
+                {
+                    Parallel.For(0, 10, i =>
+                        {
+                            C.WriteLine(ConsoleColor.Black, "firing {0}", i);
+                            m.Fire(' ', i);
+                        });
+                });
+
+
+            C.WriteLine(ConsoleColor.Black, "wait 'till done");
+            Console.ReadKey();
+
+
+            Parallel.ForEach(results, m =>
+            {
+                Parallel.For(0, 10, i =>
+                {
+                    C.WriteLine(ConsoleColor.Black, "firing {0}", i);
+                    m.Fire(' ', i);
+                });
+            });
+
+            Console.ReadKey();
+        }
+
+        private static IEnumerable<MyAsyncStateMachine> CreateMachines(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                yield return new MyAsyncStateMachine();
+            }
+        }
+    }
+}
---
+++ Stateless/SequentialActionQueue.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace Stateless
+{
+    /// <summary>Executions queued actions in sequence (but still asynchronously).</summary>
+    public class SequentialActionQueue : INotifyPropertyChanged
+    {
+        private static object syncLock = new object();
+        private Task firingTask = null;
+        private ConcurrentQueue<Action> queue = new ConcurrentQueue<Action>();
+        private bool isActive = false;
+
+        /// <summary></summary>
+        public SequentialActionQueue()
+        {
+        }
+
+        /// <summary>Raised when a property such as <see cref="IsActive"/> is changed.</summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>This property is true when the queue is processing a queued task.</summary>
+        public bool IsActive
+        {
+            get { return this.isActive; }
+            set
+            {
+                if (this.isActive != value)
+                {
+                    this.isActive = value;
+                    this.OnPropertyChanged("IsActive");
+                }
+            }
+        }
+
+        /// <summary>Thread-safe method to synchronize the asynchronous execution of the submitted action.</summary>
+        /// <param name="todo">The action to execute asynchronously.</param>
+        public void Enqueue(Action todo)
+        {
+            queue.Enqueue(todo);
+
+            lock (syncLock)
+            {
+                if (firingTask == null || firingTask.IsCompleted)
+                {
+                    firingTask = Task.Factory.StartNew(() =>
+                    {
+                        this.IsActive = true;
+
+                        Action result;
+                        while (queue.TryDequeue(out result))
+                        {
+                            result();
+                        }
+
+                        this.IsActive = false;
+                    });
+                }
+            }
+        }
+
+        private void OnPropertyChanged(string propName)
+        {
+            var e = this.PropertyChanged;
+            if (e != null)
+            {
+                e(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+
+}
